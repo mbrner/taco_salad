367eee46f8343ebd3b64b04dd6da97556411a14a
Mathis Börner <mathis.boerner@tu-dortmund.de>
2016-12-09 17:16:28 +0100

    Added option to fit in non parallel for LayerParallel

--- taco_salat/layer.py
+++ taco_salat/layer.py
@@ -40,7 +40,6 @@ class BaseLayer(object):
 
 class Layer(BaseLayer):
     def fit_df(self, df, kfold, final_model=False):
-        new_df = None
         new_df = pd.DataFrame()
         for train, test in kfold.split(np.empty(df.shape)):
             df_train = df.loc[train, :]
@@ -72,58 +71,68 @@ class Layer(BaseLayer):
 
 
 class LayerParallel(Layer):
-    def __init__(self, name, n_jobs, predict_parallel=False, comment=''):
+    def __init__(self,
+                 name,
+                 n_jobs,
+                 predict_parallel=False,
+                 fit_parallel=True,
+                 comment=''):
         super(LayerParallel, self).__init__(name=name,
                                             comment=comment)
         self.n_jobs = n_jobs
+        self.fit_parallel = fit_parallel
         self.predict_parallel = predict_parallel
 
     def fit_df(self, df, kfold, final_model=False):
-        new_df = pd.DataFrame()
+        if self.fit_parallel:
+            new_df = pd.DataFrame()
 
-        def fit_predict_single_component(component, df_train, df_test):
-            component = component.fit_df(df_train)
-            return component, component.predict_df(df_test)
+            def fit_predict_single_component(component, df_train, df_test):
+                component = component.fit_df(df_train)
+                return component, component.predict_df(df_test)
 
-        with ThreadPoolExecutor(max_workers=self.n_jobs) as executor:
-            futures = []
-            for train, test in kfold.split(np.empty(df.shape)):
-                df_train = df.loc[train, :]
-                df_test = df.loc[test, :]
-                for key, component in self.component_dict.items():
-                    has_fit = hasattr(component, 'fit_df')
-                    has_predict = hasattr(component, 'predict_df')
-                    if has_fit and has_predict:
-                        sel_att = component.attributes
-                        sel_att.append(component.label)
-                        if component.weight is not None:
-                            sel_att.append(component.weight)
-                        futures.append(executor.submit(
-                            fit_predict_single_component,
-                            component=component,
-                            df_train=df_train.loc[:, sel_att],
-                            df_test=df_test.loc[:, sel_att]))
-            results = wait(futures)
-        for i, future_i in enumerate(results.done):
-            component, comp_df = future_i.result()
-            self.component_dict[component.name] = component
-            new_df = new_df.join(comp_df)
-
-        if final_model:
             with ThreadPoolExecutor(max_workers=self.n_jobs) as executor:
-                for key, component in self.component_dict.items():
-                    has_fit = hasattr(component, 'fit_df')
-                    has_predict = hasattr(component, 'predict_df')
-                    if has_fit and has_predict:
-                        sel_att = component.attributes
-                        sel_att.append(component.label)
-                        if component.weight is not None:
-                            sel_att.append(component.weight)
-                        component.fit_df(df.loc[:, sel_att])
+                futures = []
+                for train, test in kfold.split(np.empty(df.shape)):
+                    df_train = df.loc[train, :]
+                    df_test = df.loc[test, :]
+                    for key, component in self.component_dict.items():
+                        has_fit = hasattr(component, 'fit_df')
+                        has_predict = hasattr(component, 'predict_df')
+                        if has_fit and has_predict:
+                            sel_att = component.attributes
+                            sel_att.append(component.label)
+                            if component.weight is not None:
+                                sel_att.append(component.weight)
+                            futures.append(executor.submit(
+                                fit_predict_single_component,
+                                component=component,
+                                df_train=df_train.loc[:, sel_att],
+                                df_test=df_test.loc[:, sel_att]))
                 results = wait(futures)
             for i, future_i in enumerate(results.done):
-                component = future_i.result()
+                component, comp_df = future_i.result()
                 self.component_dict[component.name] = component
+                new_df = new_df.join(comp_df)
+            if final_model:
+                with ThreadPoolExecutor(max_workers=self.n_jobs) as executor:
+                    for key, component in self.component_dict.items():
+                        has_fit = hasattr(component, 'fit_df')
+                        has_predict = hasattr(component, 'predict_df')
+                        if has_fit and has_predict:
+                            sel_att = component.attributes
+                            sel_att.append(component.label)
+                            if component.weight is not None:
+                                sel_att.append(component.weight)
+                            component.fit_df(df.loc[:, sel_att])
+                    results = wait(futures)
+                for i, future_i in enumerate(results.done):
+                    component = future_i.result()
+                    self.component_dict[component.name] = component
+        else:
+            new_df = super(LayerParallel, self).fit_df(df,
+                                                       kfold=kfold,
+                                                       final_model=final_model)
         return new_df
 
     def predict_df(self, df):
